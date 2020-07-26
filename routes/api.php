e24c7717f9324c1821597f0f8e5d6031d2177623
Abdukhaligov <hikmat.pou@gmail.com>
2020-07-26 07:19:57 +0400

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 Route::group(['middleware' => ['api']], function(){
   Route::resource('customers', 'Api\CustomerController');
   Route::resource('instances', 'Api\InstanceController');
