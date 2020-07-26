c1b4dc5caa135acb9f0e8261847aca89b0678e21
Abdukhaligov <hikmat.pou@gmail.com>
2020-07-26 07:06:57 +0400

    Update Customer.php

--- app/Http/Resources/Customer.php
+++ app/Http/Resources/Customer.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Customer extends JsonResource {
   public function toArray($request) {
-    $birthday = new DateTime($this->birthday);
     $now = new DateTime();
-    $interval = $now->diff($birthday);
+    $birthday = new DateTime($this->birthday);
+    $age = $now->diff($birthday);
+    $age = $age->y;
     $temp = new DateTime(date_format($birthday, 'Y-m-d'));
     $temp->setDate($now->format('Y'), $temp->format('m'), $temp->format('d'));
     $temp = date_diff($now, $temp);
@@ -22,7 +23,7 @@ class Customer extends JsonResource {
         "gender" => $this->gender ? "Male" : "Female",
         "birthday" => date('d-m-Y', strtotime($this->birthday)),
         "next_birthday" => !$temp->invert ? $temp->days + 1 : 'Already passed',
-        "age" => $interval->y,
+        "age" => $age,
         "created_at" => date('h:m:s d-m-Y', strtotime($this->created_at)),
         "author" => array(
             "name" => $this->author->full_name,
