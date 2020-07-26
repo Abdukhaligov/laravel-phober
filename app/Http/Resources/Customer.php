07cc6725e47a06239184f44538ae4ed1d8a950fa
Abdukhaligov <hikmat.pou@gmail.com>
2020-07-26 06:21:27 +0400

    Add Home page & modify Customer JsonResponse

--- app/Http/Resources/Customer.php
+++ app/Http/Resources/Customer.php
@@ -7,9 +7,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Customer extends JsonResource {
   public function toArray($request) {
-    $date = new DateTime($this->birthday);
+    $birthday = new DateTime($this->birthday);
     $now = new DateTime();
-    $interval = $now->diff($date);
+    $interval = $now->diff($birthday);
+    $temp = new DateTime(date_format($birthday, 'Y-m-d'));
+    $temp->setDate($now->format('Y'), $temp->format('m'), $temp->format('d'));
+    $temp = date_diff($now, $temp);
 
     return [
         "id" => $this->id,
@@ -18,6 +21,7 @@ class Customer extends JsonResource {
         "email" => $this->email,
         "gender" => $this->gender ? "Male" : "Female",
         "birthday" => date('d-m-Y', strtotime($this->birthday)),
+        "next_birthday" => !$temp->invert ? $temp->days + 1 : 'Already passed',
         "age" => $interval->y,
         "created_at" => date('h:m:s d-m-Y', strtotime($this->created_at)),
         "author" => array(
