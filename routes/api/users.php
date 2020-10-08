6b880c7b1c6d6875fd8a9cf8b7a1460a6bbd3858
HikmatAR <HikmatAR@bankrespublika.az>
2020-10-08 18:30:04 +0400

    Users api service fix

--- routes/api/users.php
+++ routes/api/users.php
@@ -5,7 +5,7 @@ Route::post('login', 'Api\UserController@login');
 Route::group(['middleware' => ['auth:api']], function(){
   Route::apiResource('users', 'Api\UserController');
 
-  Route::put('profile/update', 'Api\UserController@profileUpdate');
-  Route::post('details', 'Api\UserController@details');
+  Route::patch('profile/update', 'Api\UserController@profileUpdate');
+  Route::get('details', 'Api\UserController@details');
   Route::get('role/list', 'Api\UserController@roleList');
 });
