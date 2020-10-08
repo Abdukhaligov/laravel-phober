ed76b6f36d8c154e7958dc458e13a65b1459667a
HikmatAR <HikmatAR@bankrespublika.az>
2020-10-08 10:52:41 +0400

    Customer service fix

--- app/Http/Requests/CustomerRequest.php
+++ app/Http/Requests/CustomerRequest.php
@@ -24,11 +24,11 @@ class CustomerRequest extends FormRequest{
   public function rules(){
     return [
       'name' => ['required', 'string'],
-      'surname' =>  ['required', 'string'],
-      'phone' => ['required', 'string', 'regex: (^994(50|51|55|70|77)[0-9]{3}[0-9]{2}[0-9]{2}$)'],
-      'email' =>  ['nullable', 'email', 'unique:customers,email'],
-      'gender' =>  ['required', 'boolean'],
-      'birthday' =>  ['required', 'date']
+      'surname' => ['required', 'string'],
+      'phone' => ['required', 'string', 'regex: (^994(50|51|55|70|77)[0-9]{3}[0-9]{2}[0-9]{2}$)', 'unique:customers,phone'],
+      'email' => ['nullable', 'email', 'unique:customers,email'],
+      'gender' => ['required', 'boolean'],
+      'birthday' => ['required', 'date']
     ];
   }
 
--- app/Http/Requests/CustomerUpdateRequest.php
+++ app/Http/Requests/CustomerUpdateRequest.php
@@ -26,8 +26,10 @@ class CustomerUpdateRequest extends FormRequest{
     return [
       'name' => ['sometimes', 'required', 'string'],
       'surname' => ['sometimes', 'required', 'string'],
-      'phone' => ['sometimes', 'required', 'string', 'regex: (^994(50|51|55|70|77)[0-9]{3}[0-9]{2}[0-9]{2}$)'],
-      'email' => ['nullable', 'email', Rule::unique('customers', 'email')->ignore(substr(strrchr($this->url(), '/'), 1))],
+      'phone' => ['sometimes', 'required', 'string', 'regex: (^994(50|51|55|70|77)[0-9]{3}[0-9]{2}[0-9]{2}$)',
+        Rule::unique('customers', 'phone')->ignore(substr(strrchr($this->url(), '/'), 1))],
+      'email' => ['nullable', 'email',
+        Rule::unique('customers', 'email')->ignore(substr(strrchr($this->url(), '/'), 1))],
       'gender' => ['sometimes', 'required', 'boolean'],
       'birthday' => ['sometimes', 'required', 'date']
     ];
